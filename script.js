df7aed3ed76a7d49ea333ae1a42210ac154707b9
Myra Reeves <hif@toothfairy.com>
2023-03-22 18:45:37 -0700

    Updating CSS and JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+let question = prompt ("Is the sun still up where you are?  Type Y if it is, or N if it is not");
+console.log(question);
+
+if (question == 'Y'){
+    document.write("It's time to take Tux for a walk!  ");
+} else if (question == 'N'){
+    document.write('Puppies go to sleep now. Zzzzz...  ');
+} else {
+    document.write("Uh oh! Tux just chewed up your favorite shoes because you didn't follow the instructions!   ");
+}
+
+let introtext = '       That means it is time to learn more about Boston terriers!'
+document.write(introtext);
