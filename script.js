398c93f52312fd4c323169964e22827e27ea3d1e
Myra Reeves <hif@toothfairy.com>
2023-11-26 02:08:27 -0800

    Added note about why double quotations were used in the JS file

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//Double quotation marks are occassionally used to allow for easier/faster writing of contractions
+
 function leashes() {
-    let LeashGuesses = prompt("Your entry is blocked by an excited Tux! He wants to play a game! Can you guess what color Tux wears most often?");
+    let LeashGuesses = prompt('Your entry is blocked by an excited Tux! He wants to play a game! Can you guess what color Tux wears most often?');
 
     while (LeashGuesses != 'teal') {
         LeashGuesses = prompt("Doggie farts! That's not it. Keep guessing!  Hint: It's the name for color code #008080 written without any capitalization.");
@@ -22,15 +24,14 @@ function awake() {
     
     
     if (question == 'Y') {
-        document.write("It's time to take Tux for a walk!  ");
+        document.write("It's time to take Tux for a walk!");
     } else if (question == 'N') {
-        document.write('Puppies go to sleep now. Zzzzz...  ');
+        document.write('Puppies go to sleep now. Zzzzz...');
     } else {
         document.write("Uh oh! Tux just chewed up your favorite shoes because you didn't follow the instructions!   ");
     }    
     let introtext = '       That means it is time to learn more about Boston terriers!'
     document.write(introtext);
-
 
 }    
 
@@ -39,14 +40,10 @@ function awake() {
 
 
 function displayAdopted() {
-    let AdoptPic = +prompt("One last question. After you learn about Boston terriers, you have the option of virtual ones waiting for you at the bottom of the screen. How many virtual Boston terriers do you want to virtually adopt?");
+    let AdoptPic = +prompt('One last question. After you learn about Boston terriers, you have the option of virtual ones waiting for you at the bottom of the screen. How many virtual Boston terriers do you want to virtually adopt?');
 console.log (AdoptPic)    
     for (let i = 0; i < AdoptPic; i++) {
         document.write("<img src='adoptme.gif' alt='cartoon image of a Boston terrier with the word Adopted above it'>");
     }    
 }    
-
-
-
-
 
